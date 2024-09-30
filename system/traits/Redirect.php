b78add207f2638e1601cee701fb089fbe4153969
sadeghjfr <sadeghjfr22@gmail.com>
2024-09-30 17:09:00 +0330

    Add Redirect trait

---
+++ system/traits/Redirect.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace system\traits;
+
+trait Redirect{
+
+    protected function redirect($url){
+
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'], 'https') ? 'https://' : 'http://';
+        header("Location: " . $protocol . $_SERVER['HTTP_HOST'] . "/tinyMVC/" . $url);
+    }
+
+    protected function back(){
+
+        $httpReferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+
+        if ($httpReferer != null)
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+
+        else
+            echo "route not found";
+
+    }
+
+}
