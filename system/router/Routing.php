a2cf4d319a2a52c5dd7531a42dd310ac100f9c79
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-01 10:30:17 +0330

    Add Routing system

---
+++ system/router/Routing.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace system\router;
+use ReflectionException;
+use ReflectionMethod;
+
+class Routing{
+
+    private $currentRoute;
+
+    public function __construct(){
+
+        global $currentRoute;
+        $this->currentRoute = explode('/', $currentRoute);
+    }
+
+
+    /**
+     * @throws ReflectionException
+     */
+    public function run(){
+
+        $path = realpath(dirname(__FILE__) . "/../../application/controller/" . $this->currentRoute[0] . '.php');
+
+        if (!file_exists($path)){
+            echo "<br/>" . "404 - NOT FOUND...";
+            exit();
+        }
+
+        require_once ($path);
+        sizeof($this->currentRoute) == 1 ? $method = 'index' : $method = $this->currentRoute[1];
+
+        $class = "application\controller\\" . $this->currentRoute[0];
+        $object = new $class;
+
+        if (method_exists($object, $method)){
+
+            $reflection = new ReflectionMethod($class, $method);
+            $parameterCount = $reflection->getNumberOfParameters();
+
+            if ($parameterCount <= count(array_slice($this->currentRoute, 2)))
+                call_user_func_array(array($object, $method), array_slice($this->currentRoute, 2));
+
+            else
+                echo "404 - parameter error";
+        }
+
+        else
+            echo "404 - method not exist";
+
+
+
+
+    }
+
+
+}
