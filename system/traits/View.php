471705e177aa280be55f75c8dfb7bfa219d71085
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-01 10:31:14 +0330

    Add View trait

---
+++ system/traits/View.php
@@ -0,0 +1,23 @@
+<?php
+namespace system\traits;
+
+trait View{
+
+    protected function view($dir, $vars = null){
+
+        $dir = str_replace('.', '/', $dir);
+
+        if ($vars)
+            extract($vars);
+
+        $path = realpath(dirname(__FILE__) . "/../../application/view/" . $dir . '.php');
+
+        if (file_exists($path))
+            return require_once $path;
+
+        else
+            return "This view does not exist...";
+
+    }
+
+}
