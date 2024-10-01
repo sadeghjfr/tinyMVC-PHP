f21303f71ec1ec78385a303af85a8c4654c1baf2
sadeghjfr <sadeghjfr22@gmail.com>
2024-10-01 21:26:10 +0330

    complete config

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+
+require_once 'system/bootstrap/boot.php';
--- system/config.php
+++ system/config.php
@@ -1,1 +1,13 @@
 <?php
+
+const BASE_URL = "http://localhost/tinyMVC/";
+const BASE_DIR = "/tinyMVC/";
+
+$tmp = explode("?", $_SERVER['REQUEST_URI']);
+$currentRoute = str_replace(BASE_DIR, '', $tmp[0]);
+unset($tmp);
+
+$dbHost = "localhost";
+$dbName = "tinymvc_db";
+$dbUsername = "root";
+$dbPassword = "";
